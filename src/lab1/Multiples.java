c631db8871032b3823692f0f1935926ebb3323f3
Daniel <moyosoreokedele697@gmail.com>
2025-09-10 18:32:50 -0400

    Made multiples.

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,21 @@
 package lab1;
 
 public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples());
+    }
+
+    static int multiples(int n, int a, int b) {
+        int j = 0;
+        for (int i = 1; i < n; i++) {
+
+            // Find out which numbers divide i.
+            boolean divisibleBya = i % a == 0;
+            boolean divisibleByb = i % b == 0;
+            if (divisibleBya || divisibleByb) {
+                j++;
+            }
+        }
+        return j;
+    }
 }
